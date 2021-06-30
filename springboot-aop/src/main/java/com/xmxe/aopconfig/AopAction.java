5a1befaee70646da39dca1e303e967dba7f062a4
xmxe <464817304@qq.com>
2021-06-30 10:00:24 +0800

    springboot-aop

---
+++ springboot-aop/src/main/java/com/xmxe/aopconfig/AopAction.java
@@ -0,0 +1,30 @@
+package com.xmxe.aopconfig;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AopAction {
+	
+}
+/**
+ * @Retention 作用：表示需要在什么级别保存该注释信息，用于描述注解的生命周期（即：被描述的注解在什么范围内有效）
+ * 取值（RetentionPoicy）有：
+ * 　　　　1.SOURCE:在源文件中有效（即源文件保留）
+ * 　　　　2.CLASS:在class文件中有效（即class保留）
+ * 　　　　3.RUNTIME:在运行时有效（即运行时保留）
+ * @Target 作用：用于描述注解的使用范围（即：被描述的注解可以用在什么地方）
+ * 取值(ElementType)有：
+ * 　　　　1.CONSTRUCTOR:用于描述构造器
+ * 　　　　2.FIELD:用于描述域
+ * 　　　　3.LOCAL_VARIABLE:用于描述局部变量
+ * 　　　　4.METHOD:用于描述方法
+ * 　　　　5.PACKAGE:用于描述包
+ * 　　　　6.PARAMETER:用于描述参数
+ * 　　　　7.TYPE:用于描述类、接口(包括注解类型) 或enum声明
+ * @Inherited - 标记这个注解是继承于哪个注解类(默认 注解并没有继承于任何子类)
+ * @Documented - 标记这些注解是否包含在用户文档中。
+ */
---
+++ springboot-aop/src/main/java/com/xmxe/controller/AopController.java
@@ -0,0 +1,28 @@
+package com.xmxe.controller;
+
+import com.xmxe.aopconfig.AopAction;
+import com.xmxe.service.AopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AopController {
+
+	@Autowired
+	AopService aopService;
+
+	@AopAction//日志注解
+	@GetMapping("aop")
+	public String aop(){
+		try {
+			int c = 1/0;
+			aopService.aopAnno();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+		return "cc--oo--rr";
+	}
+
+}
