64896289acb4b902173c507a9c227f2a731a85ca
xmxe <464817304@qq.com>
2021-07-02 16:55:19 +0800

    redis distributed lock

--- springboot-redis/src/main/java/com/xmxe/util/JedisUtil.java
+++ springboot-redis/src/main/java/com/xmxe/util/JedisUtil.java
@@ -1,4 +1,4 @@
-package com.xmxe.config;
+package com.xmxe.util;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
--- springboot-redis/src/main/java/com/xmxe/util/RedisTemplateUtil.java
+++ springboot-redis/src/main/java/com/xmxe/util/RedisTemplateUtil.java
@@ -1,4 +1,4 @@
-package com.xmxe.config;
+package com.xmxe.util;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
