6087df18d5192b8500bdd87d31cfda32d261a0cb
xmxe <464817304@qq.com>
2021-07-02 14:58:20 +0800

    springboot swagger

---
+++ springboot-swagger/src/main/java/com/xmxe/config/Swagger2Configuration.java
@@ -0,0 +1,40 @@
+package com.xmxe.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class Swagger2Configuration {
+
+    @Value("${springfox.documentation.swagger.v2.enabled: true}")
+    private boolean swaggerEnable;
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(swaggerEnable)
+                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.xmxe.controller")) //这里写的是API接口所在的包位置
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("API接口文档")
+                .description("用户信息管理")
+                .version("1.0.0")
+                .build();
+    }
+
+}
